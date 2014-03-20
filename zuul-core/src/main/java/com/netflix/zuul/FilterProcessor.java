323529c374237a3d651691a096e3f5dd13bc6a31
Neeraj Joshi <njoshi@netflix.com>
2014-03-20 12:15:05 -0700

    fix parameters to DynamicCounter increment method

--- zuul-core/src/main/java/com/netflix/zuul/FilterProcessor.java
+++ zuul-core/src/main/java/com/netflix/zuul/FilterProcessor.java
@@ -190,10 +190,10 @@ public class FilterProcessor {
                     t = result.getException();
                     ctx.addFilterExecutionSummary(filterName, ExecutionStatus.FAILED.name(), execTime);
                     
-                    DynamicCounter.increment(filterName, "fail",filter.filterType());
+                    DynamicCounter.increment(filterName, "status","fail", "type",filter.filterType());
                     break;
                 case SUCCESS:
-                	DynamicCounter.increment(filterName,"success", filter.filterType());
+                	DynamicCounter.increment(filterName, "status","success", "type", filter.filterType());
                     o = result.getResult();
                     ctx.addFilterExecutionSummary(filterName, ExecutionStatus.SUCCESS.name(), execTime);
                     if (bDebug) {
@@ -202,7 +202,7 @@ public class FilterProcessor {
                     }
                     break;
                 default:
-                	DynamicCounter.increment(filterName, "unknown", filter.filterType());
+                	DynamicCounter.increment(filterName, "status", "unknown", "type", filter.filterType());
                     break;
             }
             
@@ -212,7 +212,7 @@ public class FilterProcessor {
             if (bDebug) {
                 Debug.addRoutingDebug("Running Filter failed " + filterName + " type:" + filter.filterType() + " order:" + filter.filterOrder() + " " + e.getMessage());
             }
-            DynamicCounter.increment(filterName, "fail", filter.filterType());
+            DynamicCounter.increment(filterName, "status", "fail", "type", filter.filterType());
             if (e instanceof ZuulException) {
                 throw (ZuulException) e;
             } else {
