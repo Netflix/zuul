39c7bb42dc3ad84917af49ce9379feef4dc4f60b
Nitesh Kant <nkant@netflix.com>
2014-09-18 14:28:36 -0700

    Implementing dispose on the inbound content for content that wasn't written to the client.

--- zuul-core/src/main/java/com/netflix/zuul/ZuulKaryonServer.java
+++ zuul-core/src/main/java/com/netflix/zuul/ZuulKaryonServer.java
@@ -22,7 +22,6 @@ import com.netflix.karyon.KaryonBootstrap;
 import com.netflix.karyon.archaius.ArchaiusBootstrap;
 import com.netflix.karyon.eureka.KaryonEurekaModule;
 import com.netflix.karyon.transport.http.AbstractHttpModule;
-import com.netflix.karyon.transport.http.HttpRequestHandler;
 import com.netflix.karyon.transport.http.HttpRequestRouter;
 import com.netflix.zuul.lifecycle.FilterProcessor;
 import com.netflix.zuul.lifecycle.IngressRequest;
@@ -31,16 +30,13 @@ import com.netflix.zuul.metrics.ZuulMetricsPublisherFactory;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.reactivex.netty.protocol.http.server.HttpServerBuilder;
 import io.reactivex.netty.protocol.http.server.HttpServerRequest;
 import io.reactivex.netty.protocol.http.server.HttpServerResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
-import rx.Subscription;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class ZuulKaryonServer {
 
@@ -106,85 +102,57 @@ public class ZuulKaryonServer {
             bind.toInstance(new ZuulRouter(ZuulKaryonServer.filterProcessor));
         }
 
-        @Override
-        protected HttpServerBuilder<ByteBuf, ByteBuf> newServerBuilder(int port, HttpRequestHandler<ByteBuf, ByteBuf> requestHandler) {
-            /**
-             * Why do we need to cache the content here longer?
-             *
-             * This is the processing flow:
-             *
-             *  (1) RxNetty server receives request headers -> (2) invokes Zuul's request handler ->
-             *  (3) Runs through pre-filters -> (4) Submits request to the origin (RxClient) ->
-             *  (5) RxNetty connects to origin -> (6) Write headers -> (7) Subscribes to the ServerRequest content
-             *  -> (8) Writes each content to the connection.
-             *
-             *  Since step (4) terminates the server pipeline handling for Http Server request, RxNetty will start reading
-             *  content from the inbound, potentially even before step (5) and the subscription on step (7)
-             *  If we do not cache data here, the data will be lost.
-             *  OTOH, we can not eagerly subscribe to the content as the RxClient does the subscription only after the
-             *  connection to origin is successful. The behavior of RxClient is appropriate as we do not really want to
-             *  subscribe to content before we are ready to write.
-             *  In this case, we are not requesting content from upstream on demand so we need to cache.
-             *
-             *  What happens in a streaming scenario?
-             *
-             *  We would have to leverage backpressure if we handle cases wherein the content is streamed as part of the
-             *  Server request. So, when we are overwhelmed with data (client slow) we can stop reading from upstream.
-             *
-             *  TODO: Should the timeout be configurable?
-             */
-            return super.newServerBuilder(port, requestHandler).withRequestContentSubscriptionTimeout(1, TimeUnit.MINUTES);
-        }
-    }
+        private static class ZuulRouter<State> implements HttpRequestRouter<ByteBuf, ByteBuf> {
 
-    private static class ZuulRouter<State> implements HttpRequestRouter<ByteBuf, ByteBuf> {
+            private final FilterProcessor<State> filterProcessor;
 
-        private final FilterProcessor<State> filterProcessor;
-
-        public ZuulRouter(FilterProcessor<State> filterProcessor) {
-            this.filterProcessor = filterProcessor;
-        }
-
-        @Override
-        public Observable<Void> route(HttpServerRequest<ByteBuf> request, HttpServerResponse<ByteBuf> response) {
-            final IngressRequest ingressReq = IngressRequest.from(request);
-            logger.info(request.getHttpMethod().name() + " " + request.getUri() + " " + request.getNettyChannel().remoteAddress().toString());
-            if (request.getHttpMethod().equals(HttpMethod.GET) && request.getUri().startsWith("/healthcheck")) {
-                response.setStatus(HttpResponseStatus.OK);
-                Subscription eagerSubscription = request.getContent().subscribe();
-                response.close();
-                return Observable.empty();
+            public ZuulRouter(FilterProcessor<State> filterProcessor) {
+                this.filterProcessor = filterProcessor;
             }
 
-            Observable<ByteBuf> responseContent = filterProcessor.applyAllFilters(ingressReq).
-                    flatMap(egressResp -> {
-                        response.setStatus(egressResp.getStatus());
-                        if (logger.isDebugEnabled()) {
-                            logger.debug("Setting Outgoing HTTP Status : " + egressResp.getStatus());
-                        }
-                        logger.info(egressResp.getStatus().code() + " " + request.getHttpMethod().name() + " " + request.getUri() + " " + request.getNettyChannel().remoteAddress().toString());
+            @Override
+            public Observable<Void> route(HttpServerRequest<ByteBuf> request, HttpServerResponse<ByteBuf> response) {
+                final IngressRequest ingressReq = IngressRequest.from(request);
+                logger.info(request.getHttpMethod().name() + " " + request.getUri() + " " + request.getNettyChannel()
+                                                                                                   .remoteAddress()
+                                                                                                   .toString());
+                if (request.getHttpMethod().equals(HttpMethod.GET) && request.getUri().startsWith("/healthcheck")) {
+                    response.setStatus(HttpResponseStatus.OK);
+                    return response.close();
+                }
 
-
-                        for (String headerName : egressResp.getHeaders().keySet()) {
-                            List<String> headerValues = egressResp.getHeaders().get(headerName);
+                Observable<ByteBuf> responseContent = filterProcessor.applyAllFilters(ingressReq).
+                        flatMap(egressResp -> {
+                            response.setStatus(egressResp.getStatus());
                             if (logger.isDebugEnabled()) {
-                                for (String headerValue : headerValues) {
-                                    logger.debug("Setting Outgoing HTTP Header : " + headerName + " -> " + headerValue);
-                                }
+                                logger.debug("Setting Outgoing HTTP Status : " + egressResp.getStatus());
                             }
-                            response.getHeaders().add(headerName, headerValues);
-                        }
+                            logger.info(
+                                    egressResp.getStatus().code() + " " + request.getHttpMethod().name() + " " + request
+                                            .getUri() + " " + request.getNettyChannel().remoteAddress().toString());
 
-                        return egressResp.getContent();
-                    });
 
-            Observable<Void> writeResponseContent = responseContent.map(byteBuf -> {
-                byteBuf.retain();
-                response.write(byteBuf);
-                return byteBuf;
-            }).ignoreElements().cast(Void.class).doOnCompleted(response::close);
+                            for (String headerName : egressResp.getHeaders().keySet()) {
+                                List<String> headerValues = egressResp.getHeaders().get(headerName);
+                                if (logger.isDebugEnabled()) {
+                                    for (String headerValue : headerValues) {
+                                        logger.debug(
+                                                "Setting Outgoing HTTP Header : " + headerName + " -> " + headerValue);
+                                    }
+                                }
+                                response.getHeaders().add(headerName, headerValues);
+                            }
 
-            return writeResponseContent;
+                            return egressResp.getContent();
+                        });
+
+                return responseContent.map(byteBuf -> {
+                    byteBuf.retain();
+                    response.write(byteBuf);
+                    return null;
+                }).ignoreElements().cast(Void.class).doOnCompleted(response::close);
+            }
         }
     }
 }
+
