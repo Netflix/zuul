95d16bb63facf155d2ec83cc33e48f8cb20b6c45
Matt Jacobs <mjacobs@netflix.com>
2014-09-29 10:49:13 -0700

    Accidentally missed checked in ZuulStatusCodeClass

---
+++ zuul-core/src/main/java/com/netflix/zuul/metrics/ZuulStatusCodeClass.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.zuul.metrics;
+
+import com.netflix.numerus.NumerusRollingNumberEvent;
+
+public enum ZuulStatusCodeClass implements NumerusRollingNumberEvent {
+    HTTP1xx("1xx"),
+    HTTP2xx("2xx"),
+    HTTP3xx("3xx"),
+    HTTP4xx("4xx"),
+    HTTP5xx("5xx"),
+    INIT("???");
+
+    private final String asString;
+
+    private ZuulStatusCodeClass(String s) {
+        asString = s;
+    }
+
+    @Override
+    public boolean isCounter() {
+        return true;
+    }
+
+    @Override
+    public boolean isMaxUpdater() {
+        return false;
+    }
+
+    @Override
+    public NumerusRollingNumberEvent[] getValues() {
+        return values();
+    }
+
+    public String str() {
+        return asString;
+    }
+}
