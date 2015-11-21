14109077ee7186f67549ec1fa3d2bafc3fcbec0e
Tom Breitkopf <thomas.j.breitkopf@gmail.com>
2015-11-21 16:18:58 -0500

    implemented updated AstyanaxContext methods

--- zuul-core/src/main/java/com/netflix/zuul/scriptManager/ZuulFilterDAOCassandra.java
+++ zuul-core/src/main/java/com/netflix/zuul/scriptManager/ZuulFilterDAOCassandra.java
@@ -478,7 +478,7 @@ public class ZuulFilterDAOCassandra extends Observable implements ZuulFilterDAO 
 
 
         public CassandraGatewayProd(AstyanaxContext<Keyspace> context) {
-            keyspace = context.getEntity();
+            keyspace = context.getClient();
         }
 
         public CassandraGatewayProd(Keyspace keyspace) {
