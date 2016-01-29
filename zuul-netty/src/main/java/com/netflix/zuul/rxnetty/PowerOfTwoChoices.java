612e80a32c9be2490fdbedf6fe780134e1681895
Nitesh Kant <nitesh1706@gmail.com>
2016-01-29 15:33:44 -0800

    Fix test failures.
    Since the spectator monitors were created with the same ID, they got reused across tests

--- zuul-netty/src/main/java/com/netflix/zuul/rxnetty/PowerOfTwoChoices.java
+++ zuul-netty/src/main/java/com/netflix/zuul/rxnetty/PowerOfTwoChoices.java
@@ -70,7 +70,7 @@ class PowerOfTwoChoices implements LoadBalancingStrategy<ByteBuf, ByteBuf> {
             }
 
             private HostHolder<ByteBuf, ByteBuf> selectNext() {
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 5; i++) {
                     int pos  = rand.nextInt(hosts.size());
                     HostHolder<ByteBuf, ByteBuf> first  = hosts.get(pos);
                     int pos2 = (rand.nextInt(hosts.size() - 1) + pos + 1) % hosts.size();
@@ -144,7 +144,7 @@ class PowerOfTwoChoices implements LoadBalancingStrategy<ByteBuf, ByteBuf> {
             sub.assertError(NoHostsAvailableException.class);
 
             Assert.assertEquals("Unexpected number of hosts in the pool.", 2, rule.clientMetrics.getHostsInPool().get());
-            Assert.assertEquals("Unexpected number of Unusable hosts found count.", 3,
+            Assert.assertEquals("Unexpected number of Unusable hosts found count.", 5,
                                 rule.clientMetrics.getFoundUnusableHosts().count());
             Assert.assertEquals("Unexpected number of no usable hosts count.", 1,
                                 rule.clientMetrics.getNoUsableHosts().count());
@@ -177,7 +177,7 @@ class PowerOfTwoChoices implements LoadBalancingStrategy<ByteBuf, ByteBuf> {
                 return new Statement() {
                     @Override
                     public void evaluate() throws Throwable {
-                        clientMetrics = new HttpClientMetrics("");
+                        clientMetrics = new HttpClientMetrics(StrategyRule.this.toString());
                         strategy = new PowerOfTwoChoices(clientMetrics);
                         base.evaluate();
                     }
