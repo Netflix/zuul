09740c7798190df3c8308a98790c7c054f7d153d
agonigberg <agonigberg@netflix.com>
2016-08-01 18:20:39 -0700

    remove netty modules

--- zuul-netty/src/main/java/com/netflix/zuul/rxnetty/HostSourceFactory.java
+++
@@ -1,8 +0,0 @@
-package com.netflix.zuul.rxnetty;
-
-import io.reactivex.netty.client.Host;
-import rx.Observable;
-import rx.functions.Func1;
-
-public interface HostSourceFactory extends Func1<String, Observable<Host>> {
-}
