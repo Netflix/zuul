8dbaa29fa0ef662b98f1ad78abfada02287af1c3
shuaijiang <sjzhao@travelsky.com>
2017-02-09 08:48:02 +0800

    Import the MonitoringHelper

--- zuul-core/src/main/java/com/netflix/zuul/ZuulFilter.java
+++ zuul-core/src/main/java/com/netflix/zuul/ZuulFilter.java
@@ -17,6 +17,7 @@ package com.netflix.zuul;
 
 import com.netflix.config.DynamicBooleanProperty;
 import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.zuul.monitoring.MonitoringHelper;
 import com.netflix.zuul.monitoring.Tracer;
 import com.netflix.zuul.monitoring.TracerFactory;
 import org.junit.Before;
