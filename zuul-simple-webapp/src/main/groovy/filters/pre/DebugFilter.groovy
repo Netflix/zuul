cfce07af5f83c3ba8c35667fa25fe5828c46515a
fisache <hwang031451@gmail.com>
2017-07-26 23:32:50 +0900

    Set chunkedRequestBody to carry request body

--- zuul-simple-webapp/src/main/groovy/filters/pre/DebugFilter.groovy
+++ zuul-simple-webapp/src/main/groovy/filters/pre/DebugFilter.groovy
@@ -49,6 +49,7 @@ class DebugFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext()
         ctx.setDebugRouting(true)
         ctx.setDebugRequest(true)
+        ctx.setChunkedRequestBody()
         return null;
     }
 
