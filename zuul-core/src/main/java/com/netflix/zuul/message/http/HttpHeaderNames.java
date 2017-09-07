c3c44716c4089412135204d30f04ca28f55c2233
Sudheer Vinukonda <svinukonda@netflix.com>
2017-09-07 13:14:40 -0700

    Add Access-Control-Request-Headers to HeaderNames

--- zuul-core/src/main/java/com/netflix/zuul/message/http/HttpHeaderNames.java
+++ zuul-core/src/main/java/com/netflix/zuul/message/http/HttpHeaderNames.java
@@ -87,6 +87,7 @@ public class HttpHeaderNames
     public static final HeaderName ACCESS_CONTROL_ALLOW_CREDENTIALS = HEADER_NAME_CACHE.get("Access-Control-Allow-Credentials");
     public static final HeaderName ACCESS_CONTROL_ALLOW_HEADERS = HEADER_NAME_CACHE.get("Access-Control-Allow-Headers");
     public static final HeaderName ACCESS_CONTROL_ALLOW_METHODS = HEADER_NAME_CACHE.get("Access-Control-Allow-Methods");
+    public static final HeaderName ACCESS_CONTROL_REQUEST_HEADERS = HEADER_NAME_CACHE.get("Access-Control-Request-Headers");
     public static final HeaderName STRICT_TRANSPORT_SECURITY = HEADER_NAME_CACHE.get("Strict-Transport-Security");
     public static final HeaderName LINK = HEADER_NAME_CACHE.get("Link");
 
