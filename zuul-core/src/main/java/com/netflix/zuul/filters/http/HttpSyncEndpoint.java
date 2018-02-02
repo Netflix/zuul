0be1b66a7e2fee5b22224bf0493eacb8b706988f
Mike Smith <michaels@netflix.com>
2018-02-01 17:55:43 -0800

    Change HttpSyncEndpoint to not respond until we've received the LastContent of the request
    Gate this with a property to switch back to previous behaviour so we can compare back-to-back.

--- zuul-core/src/main/java/com/netflix/zuul/filters/http/HttpSyncEndpoint.java
+++ zuul-core/src/main/java/com/netflix/zuul/filters/http/HttpSyncEndpoint.java
@@ -16,22 +16,65 @@
 
 package com.netflix.zuul.filters.http;
 
-import com.netflix.zuul.filters.SyncEndpoint;
+import com.netflix.config.CachedDynamicBooleanProperty;
+import com.netflix.zuul.filters.Endpoint;
+import com.netflix.zuul.filters.SyncZuulFilter;
 import com.netflix.zuul.message.ZuulMessage;
 import com.netflix.zuul.message.http.HttpRequestMessage;
 import com.netflix.zuul.message.http.HttpResponseMessage;
 import com.netflix.zuul.message.http.HttpResponseMessageImpl;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.LastHttpContent;
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * User: Mike Smith
  * Date: 6/16/15
  * Time: 12:23 AM
  */
-public abstract class HttpSyncEndpoint extends SyncEndpoint<HttpRequestMessage, HttpResponseMessage>
+public abstract class HttpSyncEndpoint extends Endpoint<HttpRequestMessage, HttpResponseMessage> implements SyncZuulFilter<HttpRequestMessage, HttpResponseMessage>
 {
+    // Feature flag for enabling this while we get some real data for the impact.
+    private static final CachedDynamicBooleanProperty WAIT_FOR_LASTCONTENT = new CachedDynamicBooleanProperty("zuul.endpoint.sync.wait_for_lastcontent", true);
+
+    private ZuulMessage response = null;
+    private Subscriber subscriber = null;
+
     @Override
     public HttpResponseMessage getDefaultOutput(HttpRequestMessage request)
     {
         return HttpResponseMessageImpl.defaultErrorResponse(request);
+    }
+
+    @Override
+    public Observable<HttpResponseMessage> applyAsync(HttpRequestMessage input)
+    {
+        if (WAIT_FOR_LASTCONTENT.get() && ! input.hasCompleteBody()) {
+            // Return an observable that won't complete until after we have received the LastContent from client (ie. that we've
+            // received the whole request body), so that we can't potentially corrupt the clients' http state on this connection.
+            return Observable.create(subscriber -> {
+                this.response = this.apply(input);
+                this.subscriber = subscriber;
+            });
+        }
+        else {
+            return Observable.just(this.apply(input));
+        }
+    }
+
+    @Override
+    public HttpContent processContentChunk(ZuulMessage zuulMessage, HttpContent chunk)
+    {
+        // Only call onNext() after we've received the LastContent of request from client.
+        if (chunk instanceof LastHttpContent) {
+            if (subscriber != null) {
+                subscriber.onNext(response);
+                subscriber.onCompleted();
+                response = null;
+                subscriber = null;
+            }
+        }
+        return super.processContentChunk(zuulMessage, chunk);
     }
 }
