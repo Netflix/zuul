761c34282c5cef87b94e467c49e08c84574943cf
Mike Smith <michaels@netflix.com>
2018-02-25 11:07:54 -0800

    No longer strip the x-forwarded-host header on untrusted connections, as there's no real advantage as Host header can always be faked anyway.
    And it's useful to have for debugging.

--- zuul-core/src/main/java/com/netflix/netty/common/proxyprotocol/StripUntrustedProxyHeadersHandler.java
+++ zuul-core/src/main/java/com/netflix/netty/common/proxyprotocol/StripUntrustedProxyHeadersHandler.java
@@ -44,7 +44,6 @@ public class StripUntrustedProxyHeadersHandler extends ChannelInboundHandlerAdap
 
     private static final Collection<AsciiString> HEADERS_TO_STRIP = Sets.newHashSet(
             new AsciiString("x-forwarded-for"),
-            new AsciiString("x-forwarded-host"),
             new AsciiString("x-forwarded-port"),
             new AsciiString("x-forwarded-proto"),
             new AsciiString("x-forwarded-proto-version"),
