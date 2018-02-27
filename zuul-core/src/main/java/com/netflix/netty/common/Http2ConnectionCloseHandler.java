22e5ff0725463486da722f98e0c47ed64418f667
Mike Smith <michaels@netflix.com>
2018-02-26 19:40:25 -0800

    Some fixes for Http2ConnectionCloseHandler
    * Apply on the _parent_ channel (ie. the tcp connection rather than the h2 stream channel).
    * Avoid the Http2Exception from getting logged as an error when we are only using it to signal graceful shutdown of the connection.

--- zuul-core/src/main/java/com/netflix/netty/common/Http2ConnectionCloseHandler.java
+++ zuul-core/src/main/java/com/netflix/netty/common/Http2ConnectionCloseHandler.java
@@ -17,9 +17,11 @@
 package com.netflix.netty.common;
 
 import com.netflix.config.DynamicBooleanProperty;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http2.DefaultHttp2GoAwayFrame;
 import io.netty.handler.codec.http2.Http2DataFrame;
@@ -44,6 +46,8 @@ public class Http2ConnectionCloseHandler extends ChannelOutboundHandlerAdapter
     private static final DynamicBooleanProperty ALLOW_GRACEFUL_DELAYED = new DynamicBooleanProperty(
             "server.connection.close.graceful.delayed.allow", true);
 
+    private static final SwallowHttp2ExceptionShutdownHint SWALLOW_EXCEPTION_HANDLER = new SwallowHttp2ExceptionShutdownHint();
+
     private final int gracefulCloseDelay;
 
     public Http2ConnectionCloseHandler(int gracefulCloseDelay)
@@ -51,6 +55,18 @@ public class Http2ConnectionCloseHandler extends ChannelOutboundHandlerAdapter
         this.gracefulCloseDelay = gracefulCloseDelay;
     }
 
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception
+    {
+        // Add a handler that just catches the Http2Exception that we fire to tell the codec to gracefully shutdown a connection.
+        // We want to catch it so that it doesn't get logged by the DefaultChannelPipeline as if it was a _real_
+        // exception.
+        ChannelPipeline parentPipeline = ctx.channel().parent().pipeline();
+        String handlerName = "h2_exception_swallow_handler";
+        if (parentPipeline.get(handlerName) == null) {
+            parentPipeline.addLast(handlerName, SWALLOW_EXCEPTION_HANDLER);
+        }
+    }
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception
@@ -62,7 +78,8 @@ public class Http2ConnectionCloseHandler extends ChannelOutboundHandlerAdapter
         if (isEndOfRequestResponse(msg)) {
             if (HttpChannelFlags.CLOSE_AFTER_RESPONSE.get(ctx)) {
                 promise.addListener(future -> {
-                    closeChannel(ctx, ConnectionCloseType.fromChannel(ctx.channel()), ctx.newPromise());
+                    Channel parent = parentChannel(ctx);
+                    closeChannel(ctx, parent, ConnectionCloseType.fromChannel(ctx.channel()), ctx.newPromise());
                 });
             }
         }
@@ -83,23 +100,24 @@ public class Http2ConnectionCloseHandler extends ChannelOutboundHandlerAdapter
     public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception
     {
         // Close according to the specified close type.
-        ConnectionCloseType type = ConnectionCloseType.fromChannel(ctx.channel());
-        closeChannel(ctx, type, promise);
+        Channel parent = parentChannel(ctx);
+        ConnectionCloseType type = ConnectionCloseType.fromChannel(parent);
+        closeChannel(ctx, parent, type, promise);
 
         // Don't pass this event further down the pipeline.
     }
 
-    protected void closeChannel(ChannelHandlerContext ctx, ConnectionCloseType evt, ChannelPromise promise)
+    protected void closeChannel(ChannelHandlerContext ctx, Channel channel, ConnectionCloseType evt, ChannelPromise promise)
     {
         switch (evt) {
             case DELAYED_GRACEFUL:
-                gracefullyWithDelay(ctx, promise);
+                gracefullyWithDelay(ctx, channel, promise);
                 break;
             case GRACEFUL:
-                gracefully(ctx, promise);
+                gracefully(channel, promise);
                 break;
             case IMMEDIATE:
-                immediately(ctx, promise);
+                immediately(channel, promise);
                 break;
             default:
                 throw new IllegalArgumentException("Unknown ConnectionCloseEvent type! - " + String.valueOf(evt));
@@ -116,18 +134,16 @@ public class Http2ConnectionCloseHandler extends ChannelOutboundHandlerAdapter
      *
      * See this code in okhttp where it drops response header frame if state is already shutdown:
      * https://github.com/square/okhttp/blob/master/okhttp/src/main/java/okhttp3/internal/http2/Http2Connection.java#L609
-     *
-     * @param ctx
      */
-    protected void gracefullyWithDelay(ChannelHandlerContext ctx, ChannelPromise promise)
+    protected void gracefullyWithDelay(ChannelHandlerContext ctx, Channel channel, ChannelPromise promise)
     {
         // See javadoc for explanation of why this may be disabled.
         if (! ALLOW_GRACEFUL_DELAYED.get()) {
-            gracefully(ctx, promise);
+            gracefully(channel, promise);
             return;
         }
 
-        if (isAlreadyClosing(ctx)) {
+        if (isAlreadyClosing(channel)) {
             promise.setSuccess();
             return;
         }
@@ -139,69 +155,100 @@ public class Http2ConnectionCloseHandler extends ChannelOutboundHandlerAdapter
         imminent and that initiating further requests is prohibited."
           -- https://http2.github.io/http2-spec/#GOAWAY
          */
-        final String channelId = ctx.channel().id().asShortText();
         DefaultHttp2GoAwayFrame goaway = new DefaultHttp2GoAwayFrame(Http2Error.NO_ERROR);
         goaway.setExtraStreamIds(Integer.MAX_VALUE);
-        ctx.writeAndFlush(goaway);
-        LOG.debug("gracefullyWithDelay: flushed initial go_away frame. channel=" + channelId);
+        channel.writeAndFlush(goaway);
+        LOG.debug("gracefullyWithDelay: flushed initial go_away frame. channel=" + channel.id().asShortText());
 
         // In N secs time, throw an error that causes the http2 codec to send another GOAWAY frame
         // (this time with accurate lastStreamId) and then close the connection.
         ctx.executor().schedule(() -> {
 
             // Check that the client hasn't already closed the connection (due to the earlier goaway we sent).
-            if (ctx.channel().isActive()) {
-                LOG.debug("gracefullyWithDelay: firing graceful_shutdown event to make netty send a final go_away frame and then close connection. channel=" + channelId);
-                Http2Exception h2e = new Http2Exception(Http2Error.NO_ERROR, Http2Exception.ShutdownHint.GRACEFUL_SHUTDOWN);
-                ctx.pipeline().fireExceptionCaught(h2e);
-            }
-            else {
-                LOG.debug("gracefullyWithDelay: connection already closed, so no need to send final go_away frame. channel=" + channelId);
-            }
+            gracefulConnectionShutdown(channel);
             promise.setSuccess();
 
         }, gracefulCloseDelay, TimeUnit.SECONDS);
     }
 
-    protected void gracefully(ChannelHandlerContext ctx, ChannelPromise promise)
+    protected void gracefulConnectionShutdown(Channel channel)
     {
-        if (isAlreadyClosing(ctx)) {
+        if (channel.isActive()) {
+            LOG.debug("gracefullyWithDelay: firing graceful_shutdown event to make netty send a final go_away frame and then close connection. channel="
+                    + channel.id().asShortText());
+            Http2Exception h2e = new Http2Exception(Http2Error.NO_ERROR, Http2Exception.ShutdownHint.GRACEFUL_SHUTDOWN);
+            channel.pipeline().fireExceptionCaught(h2e);
+        }
+        else {
+            LOG.debug("gracefullyWithDelay: connection already closed, so no need to send final go_away frame. channel=" + channel.id().asShortText());
+        }
+    }
+
+    protected void gracefully(Channel channel, ChannelPromise promise)
+    {
+        if (isAlreadyClosing(channel)) {
             promise.setSuccess();
             return;
         }
 
-        if (ctx.channel().isActive()) {
-            Http2Exception h2e = new Http2Exception(Http2Error.NO_ERROR, Http2Exception.ShutdownHint.GRACEFUL_SHUTDOWN);
-            ctx.pipeline().fireExceptionCaught(h2e);
-        }
+        gracefulConnectionShutdown(channel);
         promise.setSuccess();
     }
 
-    protected void immediately(ChannelHandlerContext ctx, ChannelPromise promise)
+    protected void immediately(Channel parent, ChannelPromise promise)
     {
-        if (isAlreadyClosing(ctx)) {
+        if (isAlreadyClosing(parent)) {
             promise.setSuccess();
             return;
         }
 
-        if (ctx.channel().isActive()) {
-            ctx.close(promise);
+        if (parent.isActive()) {
+            parent.close(promise);
         }
         else {
             promise.setSuccess();
         }
     }
 
-    protected boolean isAlreadyClosing(ChannelHandlerContext ctx)
+    protected boolean isAlreadyClosing(Channel parentChannel)
     {
         // If already closing, then just return.
         // This will happen because close() is called a 2nd time after sending the goaway frame.
-        if (HttpChannelFlags.CLOSING.get(ctx)) {
+        if (HttpChannelFlags.CLOSING.get(parentChannel)) {
             return true;
         }
         else {
-            HttpChannelFlags.CLOSING.set(ctx);
+            HttpChannelFlags.CLOSING.set(parentChannel);
             return false;
+        }
+    }
+
+    protected Channel parentChannel(ChannelHandlerContext ctx) {
+        return ctx.channel().parent();
+    }
+
+
+    @Sharable
+    static class SwallowHttp2ExceptionShutdownHint extends ChannelOutboundHandlerAdapter
+    {
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+        {
+            if (cause instanceof Http2Exception) {
+                Http2Exception h2e = (Http2Exception) cause;
+                if (h2e.error() == Http2Error.NO_ERROR
+                        && Http2Exception.ShutdownHint.GRACEFUL_SHUTDOWN.equals(h2e.shutdownHint())) {
+                    // This is the exception we threw ourselves to make the http2 codec gracefully close the connection. So just
+                    // swallow it so that it doesn't propagate and get logged.
+                }
+                else {
+                    super.exceptionCaught(ctx, cause);
+                }
+            }
+            else {
+                super.exceptionCaught(ctx, cause);
+            }
+
         }
     }
 }
