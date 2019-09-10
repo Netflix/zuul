e1c7481e5ab52f0903fb10dcc0b29c1ab4ed7c77
Carl Mastrangelo <carl-mastrangelo@users.noreply.github.com>
2019-09-09 17:23:40 -0700

    core: make sure not to fire HAProxy messages on completion

--- zuul-core/src/main/java/com/netflix/netty/common/proxyprotocol/ElbProxyProtocolChannelHandler.java
+++ zuul-core/src/main/java/com/netflix/netty/common/proxyprotocol/ElbProxyProtocolChannelHandler.java
@@ -74,6 +74,7 @@ public class ElbProxyProtocolChannelHandler extends ChannelInboundHandlerAdapter
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
     {
+        boolean stillRead = true;
         if (withProxyProtocol) {
             if (msg instanceof HAProxyMessage && msg != null) {
                 HAProxyMessage hapm = (HAProxyMessage) msg;
@@ -106,6 +107,12 @@ public class ElbProxyProtocolChannelHandler extends ChannelInboundHandlerAdapter
                         haProxyTLV.release();
                     }
                 }
+                stillRead = false;
+                if (hapm.refCnt() > 0) {
+                    hapm.release();
+                } else {
+                    logger.warn("Unexpected ref count on HAProxyMessage");
+                }
 
                 // TODO - fire an additional event to notify interested parties that we now know the IP?
 
@@ -113,7 +120,8 @@ public class ElbProxyProtocolChannelHandler extends ChannelInboundHandlerAdapter
                 ctx.pipeline().remove(this);
             }
         }
-
-        super.channelRead(ctx, msg);
+        if (stillRead) {
+            super.channelRead(ctx, msg);
+        }
     }
 }
