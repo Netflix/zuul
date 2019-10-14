f7d3933ede4512fb2749348cf0a33a3d9adad79c
Arthur Gonigberg <agonigberg@netflix.com>
2019-10-14 11:52:34 -0700

    modfy to use non-port SSL info

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/http2/Http2Configuration.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/http2/Http2Configuration.java
@@ -32,6 +32,10 @@ public class Http2Configuration {
 
 
     public static SslContext configureSSL(SslContextFactory sslContextFactory, int port) {
+        return configureSSL(sslContextFactory, String.valueOf(port));
+    }
+
+    public static SslContext configureSSL(SslContextFactory sslContextFactory, String metricId) {
         SslContextBuilder builder = sslContextFactory.createBuilderForServer();
 
         String[] supportedProtocol;
@@ -65,7 +69,7 @@ public class Http2Configuration {
         sslContextFactory.enableSessionTickets(sslContext);
 
         // Setup metrics tracking the OpenSSL stats.
-        sslContextFactory.configureOpenSslStatsMetrics(sslContext, Integer.toString(port));
+        sslContextFactory.configureOpenSslStatsMetrics(sslContext, metricId);
 
         return sslContext;
     }
