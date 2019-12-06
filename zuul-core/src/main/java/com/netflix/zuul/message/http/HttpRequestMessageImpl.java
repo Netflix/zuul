134ef192bfefff465543457e78384c60109b8160
Carl Mastrangelo <cmastrangelo@netflix.com>
2019-12-05 17:47:28 -0800

    zuul-core,zuul-sample: avoid using deprecated cookie apis

--- zuul-core/src/main/java/com/netflix/zuul/message/http/HttpRequestMessageImpl.java
+++ zuul-core/src/main/java/com/netflix/zuul/message/http/HttpRequestMessageImpl.java
@@ -27,9 +27,9 @@ import com.netflix.zuul.message.Headers;
 import com.netflix.zuul.message.ZuulMessage;
 import com.netflix.zuul.message.ZuulMessageImpl;
 import com.netflix.zuul.util.HttpUtils;
-import io.netty.handler.codec.http.Cookie;
-import io.netty.handler.codec.http.CookieDecoder;
 import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -354,16 +354,15 @@ public class HttpRequestMessageImpl implements HttpRequestMessage
                     aCookieHeader = cleanCookieHeader(aCookieHeader);
                 }
 
-                Set<Cookie> decoded = CookieDecoder.decode(aCookieHeader, false);
+                Set<Cookie> decoded = ServerCookieDecoder.LAX.decode(aCookieHeader);
                 for (Cookie cookie : decoded) {
                     cookies.add(cookie);
                 }
             }
             catch (Exception e) {
                 LOG.error(String.format("Error parsing request Cookie header. cookie=%s, request-info=%s",
-                        aCookieHeader, getInfoForLogging()));
+                        aCookieHeader, getInfoForLogging()), e);
             }
-
         }
         parsedCookies = cookies;
         return cookies;
