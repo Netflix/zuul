a58f4c927e7906aad597954190e49206be1afcb9
Cody Rioux <crioux@netflix.com>
2020-01-28 13:57:57 -0800

    ZuulFiltersModule now injects AbstractConfiguration.
    ZuulFiltersModule used to statically load AbstractConfiguration via
    ConfigurationManager.getConfigInstance(). This limits the user's ability
    to bind various configuration loading mechanisms via the injector.

--- zuul-sample/src/main/java/com/netflix/zuul/sample/Bootstrap.java
+++ zuul-sample/src/main/java/com/netflix/zuul/sample/Bootstrap.java
@@ -17,7 +17,6 @@
 package com.netflix.zuul.sample;
 
 import com.google.inject.Injector;
-import com.netflix.config.ConfigurationManager;
 import com.netflix.governator.InjectorBuilder;
 import com.netflix.zuul.netty.server.BaseServerStartup;
 import com.netflix.zuul.netty.server.Server;
@@ -42,7 +41,6 @@ public class Bootstrap {
         Server server = null;
 
         try {
-            ConfigurationManager.loadCascadedPropertiesFromResources("application");
             Injector injector = InjectorBuilder.fromModule(new ZuulSampleModule()).createInjector();
             BaseServerStartup serverStartup = injector.getInstance(BaseServerStartup.class);
             server = serverStartup.server();
