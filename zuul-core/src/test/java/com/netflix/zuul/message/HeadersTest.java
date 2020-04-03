a7bd3fb17a37f561c8015320d3e097707e122d99
Carl Mastrangelo <cmastrangelo@netflix.com>
2020-04-03 01:22:16 -0700

    Add some tests

--- zuul-core/src/test/java/com/netflix/zuul/message/HeadersTest.java
+++ zuul-core/src/test/java/com/netflix/zuul/message/HeadersTest.java
@@ -17,9 +17,13 @@
 package com.netflix.zuul.message;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.truth.Truth;
 import java.util.List;
+import java.util.Set;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -29,6 +33,389 @@ import org.junit.runners.JUnit4;
  */
 @RunWith(JUnit4.class)
 public class HeadersTest {
+
+    @Test
+    public void copyOf() {
+        Headers headers = new Headers();
+        headers.set("Content-Length", "5");
+        Headers headers2 = Headers.copyOf(headers);
+        headers2.add("Via", "duct");
+
+        Truth.assertThat(headers.getAll("Via")).isEmpty();
+    }
+
+    @Test
+    public void getFirst_normalizesName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getFirst("cOOkIE")).isEqualTo("this=that");
+    }
+
+    @Test
+    public void getFirst_headerName_normalizesName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getFirst(new HeaderName("cOOkIE"))).isEqualTo("this=that");
+    }
+
+    @Test
+    public void getFirst_returnsNull() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getFirst("Date")).isNull();
+    }
+
+    @Test
+    public void getFirst_headerName_returnsNull() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getFirst(new HeaderName("Date"))).isNull();
+    }
+
+    @Test
+    public void getFirst_returnsDefault() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getFirst("Date", "tuesday")).isEqualTo("tuesday");
+    }
+
+    @Test
+    public void getFirst_headerName_returnsDefault() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getFirst(new HeaderName("Date"), "tuesday")).isEqualTo("tuesday");
+    }
+
+    @Test
+    public void getAll() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getAll("CookiE")).containsExactly("this=that", "frizzle=frazzle").inOrder();
+    }
+
+    @Test
+    public void getAll_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Truth.assertThat(headers.getAll(new HeaderName("CookiE")))
+                .containsExactly("this=that", "frizzle=frazzle").inOrder();
+    }
+
+    @Test
+    public void setClearsExisting() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.set("cookIe", "dilly=dally");
+
+        Truth.assertThat(headers.getAll("CookiE")).containsExactly("dilly=dally");
+        Truth.assertThat(headers.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void setClearsExisting_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.set(new HeaderName("cookIe"), "dilly=dally");
+
+        Truth.assertThat(headers.getAll("CookiE")).containsExactly("dilly=dally");
+        Truth.assertThat(headers.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void setNullIsEmtpy() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.set("cookIe", null);
+
+        Truth.assertThat(headers.getAll("CookiE")).isEmpty();
+        Truth.assertThat(headers.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void setNullIsEmtpy_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.set(new HeaderName("cookIe"), null);
+
+        Truth.assertThat(headers.getAll("CookiE")).isEmpty();
+        Truth.assertThat(headers.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void setIfAbsentKeepsExisting() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.setIfAbsent("cookIe", "dilly=dally");
+
+        Truth.assertThat(headers.getAll("CookiE")).containsExactly("this=that", "frizzle=frazzle").inOrder();
+    }
+
+    @Test
+    public void setIfAbsentKeepsExisting_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.setIfAbsent(new HeaderName("cookIe"), "dilly=dally");
+
+        Truth.assertThat(headers.getAll("CookiE")).containsExactly("this=that", "frizzle=frazzle").inOrder();
+    }
+
+    @Test
+    public void setIfAbsentFailsOnNull() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        assertThrows(NullPointerException.class, () -> headers.setIfAbsent("cookIe", null));
+    }
+
+    @Test
+    public void setIfAbsentFailsOnNull_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        assertThrows(NullPointerException.class, () -> headers.setIfAbsent(new HeaderName("cookIe"), null));
+    }
+
+    @Test
+    public void setIfAbsent() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.setIfAbsent("X-Netflix-Awesome", "true");
+
+        Truth.assertThat(headers.getAll("X-netflix-Awesome")).containsExactly("true");
+    }
+
+    @Test
+    public void setIfAbsent_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.setIfAbsent(new HeaderName("X-Netflix-Awesome"), "true");
+
+        Truth.assertThat(headers.getAll("X-netflix-Awesome")).containsExactly("true");
+    }
+
+    @Test
+    public void add() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.add("via", "con Dios");
+
+        Truth.assertThat(headers.getAll("Via")).containsExactly("duct", "con Dios").inOrder();
+    }
+
+    @Test
+    public void add_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        headers.add(new HeaderName("via"), "con Dios");
+
+        Truth.assertThat(headers.getAll("Via")).containsExactly("duct", "con Dios").inOrder();
+    }
+
+    @Test
+    public void putAll() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+
+        Headers other = new Headers();
+        other.add("cookie", "a=b");
+        other.add("via", "com");
+
+        headers.putAll(other);
+
+        // Only check the order per field, not for the entire set.
+        Truth.assertThat(headers.getAll("Via")).containsExactly("duct", "com").inOrder();
+        Truth.assertThat(headers.getAll("coOkiE")).containsExactly("this=that", "frizzle=frazzle", "a=b").inOrder();
+        Truth.assertThat(headers.size()).isEqualTo(5);
+    }
+
+
+    @Test
+    public void remove() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        List<String> removed = headers.remove("Cookie");
+
+        Truth.assertThat(headers.getAll("Cookie")).isEmpty();
+        Truth.assertThat(headers.getAll("Soup")).containsExactly("salad");
+        Truth.assertThat(headers.size()).isEqualTo(2);
+        Truth.assertThat(removed).containsExactly("this=that", "frizzle=frazzle").inOrder();
+    }
+
+    @Test
+    public void remove_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        List<String> removed = headers.remove(new HeaderName("Cookie"));
+
+        Truth.assertThat(headers.getAll("Cookie")).isEmpty();
+        Truth.assertThat(headers.getAll("Soup")).containsExactly("salad");
+        Truth.assertThat(headers.size()).isEqualTo(2);
+        Truth.assertThat(removed).containsExactly("this=that", "frizzle=frazzle").inOrder();
+    }
+
+    @Test
+    public void removeEmpty() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        List<String> removed = headers.remove("Monkey");
+
+        Truth.assertThat(headers.getAll("Cookie")).isNotEmpty();
+        Truth.assertThat(headers.getAll("Soup")).containsExactly("salad");
+        Truth.assertThat(headers.size()).isEqualTo(4);
+        Truth.assertThat(removed).isEmpty();
+    }
+
+    @Test
+    public void removeEmpty_headerName() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("Cookie", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        List<String> removed = headers.remove(new HeaderName("Monkey"));
+
+        Truth.assertThat(headers.getAll("Cookie")).isNotEmpty();
+        Truth.assertThat(headers.getAll("Soup")).containsExactly("salad");
+        Truth.assertThat(headers.size()).isEqualTo(4);
+        Truth.assertThat(removed).isEmpty();
+    }
+
+    @Test
+    public void removeIf() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("Cookie", "this=that");
+        headers.add("COOkie", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        boolean removed = headers.removeIf(entry -> entry.getKey().getName().equals("Cookie"));
+
+        assertTrue(removed);
+        Truth.assertThat(headers.getAll("cOoKie")).containsExactly("frizzle=frazzle");
+        Truth.assertThat(headers.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void keySet() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("COOKie", "this=that");
+        headers.add("cookIE", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        Set<HeaderName> keySet = headers.keySet();
+
+        Truth.assertThat(keySet)
+                .containsExactly(new HeaderName("COOKie"), new HeaderName("Soup"), new HeaderName("Via"));
+        for (HeaderName headerName : keySet) {
+            if (headerName.getName().equals("COOKie")) {
+                return;
+            }
+        }
+        throw new AssertionError("didn't find right cookie in keys: " + keySet);
+    }
+
+    @Test
+    public void contains() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("COOKie", "this=that");
+        headers.add("cookIE", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        assertTrue(headers.contains("CoOkIe"));
+        assertTrue(headers.contains(new HeaderName("CoOkIe")));
+        assertFalse(headers.contains("Monkey"));
+        assertFalse(headers.contains(new HeaderName("Monkey")));
+    }
+
+    @Test
+    public void containsValue() {
+        Headers headers = new Headers();
+        headers.add("Via", "duct");
+        headers.add("COOKie", "this=that");
+        headers.add("cookIE", "frizzle=frazzle");
+        headers.add("Soup", "salad");
+
+        // note the swapping of the two cookie casings.
+        assertTrue(headers.contains("CoOkIe", "frizzle=frazzle"));
+        assertTrue(headers.contains(new HeaderName("CoOkIe"), "frizzle=frazzle"));
+        assertFalse(headers.contains("Via", "lin"));
+        assertFalse(headers.contains(new HeaderName("Soup"), "of the day"));
+    }
 
     @Test
     public void testCaseInsensitiveKeys_Set() {
