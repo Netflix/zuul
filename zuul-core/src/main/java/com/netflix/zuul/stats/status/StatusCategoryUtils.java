e0d3fcf841bb897a338376bc4eca1d6c9acbb236
argha-c <007.argha@gmail.com>
2020-07-23 15:51:01 -0700

    Fix variable typo. Remove unused code

--- zuul-core/src/main/java/com/netflix/zuul/stats/status/StatusCategoryUtils.java
+++ zuul-core/src/main/java/com/netflix/zuul/stats/status/StatusCategoryUtils.java
@@ -19,9 +19,6 @@ package com.netflix.zuul.stats.status;
 import com.netflix.zuul.context.CommonContextKeys;
 import com.netflix.zuul.context.SessionContext;
 import com.netflix.zuul.message.ZuulMessage;
-import com.netflix.zuul.message.http.HttpResponseMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * User: michaels@netflix.com
@@ -29,31 +26,17 @@ import org.slf4j.LoggerFactory;
  * Time: 2:48 PM
  */
 public class StatusCategoryUtils {
-    private static final Logger LOG = LoggerFactory.getLogger(StatusCategoryUtils.class);
 
     public static StatusCategory getStatusCategory(ZuulMessage msg) {
         return getStatusCategory(msg.getContext());
     }
 
     public static StatusCategory getStatusCategory(SessionContext ctx) {
-        return (StatusCategory) ctx.get(CommonContextKeys.STATUS_CATGEORY);
+        return (StatusCategory) ctx.get(CommonContextKeys.STATUS_CATEGORY);
     }
 
     public static void setStatusCategory(SessionContext ctx, StatusCategory statusCategory) {
-        ctx.set(CommonContextKeys.STATUS_CATGEORY, statusCategory);
-    }
-
-    public static StatusCategory getOriginStatusCategory(SessionContext ctx) {
-        return (StatusCategory) ctx.get(CommonContextKeys.ORIGIN_STATUS_CATEGORY);
-    }
-
-    public static boolean isResponseHttpErrorStatus(HttpResponseMessage response) {
-        boolean isHttpError = false;
-        if (response != null) {
-            int status = response.getStatus();
-            isHttpError = isResponseHttpErrorStatus(status);
-        }
-        return isHttpError;
+        ctx.set(CommonContextKeys.STATUS_CATEGORY, statusCategory);
     }
 
     public static boolean isResponseHttpErrorStatus(int status) {
@@ -62,9 +45,9 @@ public class StatusCategoryUtils {
 
     public static void storeStatusCategoryIfNotAlreadyFailure(final SessionContext context, final StatusCategory statusCategory) {
         if (statusCategory != null) {
-            final StatusCategory nfs = (StatusCategory) context.get(CommonContextKeys.STATUS_CATGEORY);
+            final StatusCategory nfs = (StatusCategory) context.get(CommonContextKeys.STATUS_CATEGORY);
             if (nfs == null || nfs.getGroup().getId() == ZuulStatusCategoryGroup.SUCCESS.getId()) {
-                context.set(CommonContextKeys.STATUS_CATGEORY, statusCategory);
+                context.set(CommonContextKeys.STATUS_CATEGORY, statusCategory);
             }
         }
     }
