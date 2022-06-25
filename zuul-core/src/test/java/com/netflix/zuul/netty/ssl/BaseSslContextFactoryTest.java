030f21eda49cfe9208175dbea4008cad5d335f56
sullis <seansullivan@netflix.com>
2022-06-25 14:35:21 -0700

    verify that default SSL provider is OpenSSL (#1267)

---
+++ zuul-core/src/test/java/com/netflix/zuul/netty/ssl/BaseSslContextFactoryTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 Netflix, Inc.
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+
+package com.netflix.zuul.netty.ssl;
+
+import static org.junit.Assert.assertEquals;
+
+import io.netty.handler.ssl.SslProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+/**
+ * Tests for {@link BaseSslContextFactory}.
+ */
+@RunWith(JUnit4.class)
+public class BaseSslContextFactoryTest {
+    @Test
+    public void testDefaultSslProviderIsOpenSsl() {
+        assertEquals(SslProvider.OPENSSL, BaseSslContextFactory.chooseSslProvider());
+    }
+}
