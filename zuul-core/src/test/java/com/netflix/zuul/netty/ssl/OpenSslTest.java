b6e056b893d3d183b069fd80dd0a88a09e6f6dcb
Sean C. Sullivan <github@seansullivan.com>
2023-02-04 18:34:08 -0800

    fix OpenRewrite violation

--- zuul-core/src/test/java/com/netflix/zuul/netty/ssl/OpenSslTest.java
+++ zuul-core/src/test/java/com/netflix/zuul/netty/ssl/OpenSslTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class OpenSslTest {
+class OpenSslTest {
     @BeforeEach
     void beforeEach() {
         OpenSsl.ensureAvailability();
