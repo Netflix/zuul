f8717548bd2327fcbc5cd54a765a1aa070d0dffd
Justin Guerra <jguerra@netflix.com>
2023-10-20 13:41:50 -0600

    More pr feedback

--- zuul-core/src/main/java/com/netflix/netty/common/proxyprotocol/ElbProxyProtocolChannelHandler.java
+++ zuul-core/src/main/java/com/netflix/netty/common/proxyprotocol/ElbProxyProtocolChannelHandler.java
@@ -64,7 +64,6 @@ public final class ElbProxyProtocolChannelHandler extends ChannelInboundHandlerA
                     .attr(SourceAddressChannelHandler.ATTR_SERVER_LOCAL_PORT)
                     .get();
 
-            String needsMoreData = String.valueOf(haProxyState == ProtocolDetectionState.NEEDS_MORE_DATA);
             // This likely means initialization was requested with proxy protocol, but we encountered a non-ppv2
             // message
             registry.counter(
@@ -74,7 +73,7 @@ public final class ElbProxyProtocolChannelHandler extends ChannelInboundHandlerA
                             "port",
                             String.valueOf(port),
                             "needs_more_data",
-                            needsMoreData)
+                            String.valueOf(haProxyState == ProtocolDetectionState.NEEDS_MORE_DATA))
                     .increment();
             ctx.pipeline().remove(this);
         }
