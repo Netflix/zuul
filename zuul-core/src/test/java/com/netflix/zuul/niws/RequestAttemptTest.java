569415ae1d08e4f1ad4caaee2ea6cadf4b1bb761
Gavin Bunney <409207+gavinbunney@users.noreply.github.com>
2024-01-30 09:39:51 -0800

    Only capture first line of stacktrace for unexpected RequestAttempt exceptions (#1736)

--- zuul-core/src/test/java/com/netflix/zuul/niws/RequestAttemptTest.java
+++ zuul-core/src/test/java/com/netflix/zuul/niws/RequestAttemptTest.java
@@ -18,6 +18,9 @@ package com.netflix.zuul.niws;
 
 import com.netflix.zuul.exception.OutboundErrorType;
 import com.netflix.zuul.netty.connectionpool.OriginConnectException;
+import io.netty.handler.codec.http2.DefaultHttp2Connection;
+import io.netty.handler.codec.http2.Http2Error;
+import io.netty.handler.codec.http2.Http2Exception;
 import org.junit.jupiter.api.Test;
 
 import javax.net.ssl.SSLHandshakeException;
@@ -26,6 +29,7 @@ import java.security.cert.CertificateException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 public class RequestAttemptTest {
@@ -76,5 +80,39 @@ public class RequestAttemptTest {
 
         assertEquals("ORIGIN_CONNECT_ERROR", attempt.getError());
         assertEquals("java.lang.RuntimeException: socket failure", attempt.getCause());
+    }
+
+    @Test
+    void h2ExceptionCauseHandled() {
+        // mock out a real-ish h2 stream exception
+        Exception h2Exception = spy(Http2Exception.streamError(
+                100,
+                Http2Error.REFUSED_STREAM,
+                "Cannot create stream 100 greater than Last-Stream-ID 99 from GOAWAY.",
+                new Object[] {100, 99}));
+
+        // mock a stacktrace to ensure we don't actually capture it completely
+        when(h2Exception.getStackTrace()).thenReturn(new StackTraceElement[] {
+            new StackTraceElement(
+                    DefaultHttp2Connection.class.getCanonicalName(),
+                    "createStream",
+                    "DefaultHttp2Connection.java",
+                    772),
+            new StackTraceElement(
+                    DefaultHttp2Connection.class.getCanonicalName(),
+                    "checkNewStreamAllowed",
+                    "DefaultHttp2Connection.java",
+                    902)
+        });
+
+        RequestAttempt attempt = new RequestAttempt(1, null, null, "target", "chosen", 200, null, null, 0, 0, 0);
+        attempt.setException(h2Exception);
+
+        assertEquals("Cannot create stream 100 greater than Last-Stream-ID 99 from GOAWAY.", attempt.getError());
+        assertEquals("StreamException", attempt.getExceptionType());
+
+        assertEquals(
+                "io.netty.handler.codec.http2.DefaultHttp2Connection.createStream(DefaultHttp2Connection.java:772)",
+                attempt.getCause());
     }
 }
